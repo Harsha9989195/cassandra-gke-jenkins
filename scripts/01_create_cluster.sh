cd5f3c9cbb79209faf00c5245c52c764505c3d9e
Harsha <ntr9100@gmail.com>
2025-08-21 14:03:31 +0530

    Update 01_create_cluster.sh

--- scripts/01_create_cluster.sh
+++ scripts/01_create_cluster.sh
@@ -7,12 +7,14 @@ ZONE="${ZONE:-us-central1-a}"
 echo "Using project: $PROJECT_ID  region: $REGION  zone: $ZONE"
 gcloud services enable container.googleapis.com
 
-gcloud container clusters create cassandra-gke
---zone "$ZONE"
---num-nodes 1
---machine-type e2-standard-2
---disk-type pd-ssd --disk-size 50
---labels=workload=cassandra
+gcloud container clusters create cassandra-gke \
+  --project "$PROJECT_ID" \
+  --zone "$ZONE" \
+  --num-nodes 1 \
+  --machine-type e2-standard-2 \
+  --disk-type pd-ssd --disk-size 50 \
+  --labels=workload=cassandra
+
 
 gcloud container clusters get-credentials cassandra-gke --zone "$ZONE"
 kubectl get nodes
